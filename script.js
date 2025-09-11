e6b858e54b7a0189c7b8927a2c6f59f9ca9a178a
GiovannaOliveira18 <giovanna.oliveira1@unesp.br>
2025-09-11 16:33:41 -0300

    one more

--- script.js
+++ script.js
@@ -20,4 +20,13 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('header nav');
+
+    menuToggle.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  });
+
 
